2dd51a9ed5e0575e105192cdc25466512d58e1cd
clif edwards <clif.edwards.2@gmail.com>
2017-04-06 07:55:16 -0400

    [ REFACT test/EVOLVE_this Name changed to EVOLVE_RCBounds] EVOLVE_RCBounds = EVOLVE_this( R.__, default_RCBounds)

--- RC/test/EVOLVE_RCBounds.js
+++ RC/test/EVOLVE_RCBounds.js
@@ -16,32 +16,32 @@ let chai = require('chai')
     , expect = chai.expect
 ;
 
-// CODE UNDER TEST// todo _> add f(focusNdx) to affect the transforms that EVOLVE_RCBounds USES. 170407 @ 1950
-let EVOLVE_RCBounds;
+let EVOLVE_this;
 let transforms;
-EVOLVE_RCBounds = (trans, dict) => R.evolve(trans, dict); // DICT -> DICT
+EVOLVE_this = (alterFn, dict) => R.evolve(alterFn, dict); // DICT -> DICT
 
-describe(`the Fn: EVOLVE_RCBounds( Fn:transforms ) -> RCB
+describe(`the Fn: EVOLVE_this( alterFn )(default_RCB) -> newRCB
     
-    // USAGE: EVOLVE_RCBounds WITH the defaultRCBounds partialed, EVOLVES it WITH the transforms Object. It is a f(focusNdx) 
+    USAGE: EVOLVE_RCBounds = EVOLVE_this(WITH a partialed default_RCBounds argument) -> new_RCBounds
+    then EVOLVE_RCBounds( with an alterFn)  
     `, function () {
     beforeEach(function () {
-        this.defaultRCB = {focus: {beg: 0, len: 2}};
-        this.CUT = EVOLVE_RCBounds(R.__, this.defaultRCB);
+        this.default_RCB = {focus: {beg: 0, len: 2}};
+        this.CUT = EVOLVE_this(R.__, this.default_RCB);
     });
-    describe(`CONFIRM [defaultRCB] the defaultReadClssBounds IS Valid.
+    describe(`CONFIRM [default_RCB] the defaultReadClssBounds IS Valid.
     `, function () {
         it(`should be a DICTionary with at least 2 keys.
     `, function () {
-            expect(this.defaultRCB.focus).to.be.an('object').and.to.have.all.keys(['beg', 'len']);
+            expect(this.default_RCB.focus).to.be.an('object').and.to.have.all.keys(['beg', 'len']);
         });
     });
-    describe(`CONFIRM EVOLVE_RCBounds is a function wanting a Transforms Fn.`, function () {
+    describe(`CONFIRM EVOLVE_this is a function wanting a Transforms Fn.`, function () {
         it(`should be a function with one argument.    `, function () {
             expect(this.CUT).to.be.a('function').and.to.have.lengthOf(1);
         });
     });
-    describe(`CONFIRM EVOLVE_RCBounds(aTransforms) CAN ALTER the RCBounds DICT.    `, function () {
+    describe(`CONFIRM EVOLVE_this(aTransforms) CAN alterFn the RCBounds DICT.    `, function () {
         it(`can return an altered RCBounds `, function () {
             transforms = {focus: {beg: R.add(20)}};
             expect(this.CUT(transforms).focus).to.be.an('object').and.to.have.all.keys(['beg', 'len']);
@@ -51,7 +51,7 @@ describe(`the Fn: EVOLVE_RCBounds( Fn:transforms ) -> RCB
             expect(this.CUT(transforms).focus.beg).to.equal(20);
         });
     });
-    describe(`CONFIRM EVOLVE_RCBounds(aTransforms W/O AN EXISTING Key) WILL NOT ALTER the RCBounds DICT.    `, function () {
+    describe(`CONFIRM EVOLVE_this(aTransforms W/O AN EXISTING Key) WILL NOT ALTER the RCBounds DICT.    `, function () {
         it(`should see UNALTERED key:value    `, function () {
             transforms = {focus: {WRONG: R.always(20)}};
             //
