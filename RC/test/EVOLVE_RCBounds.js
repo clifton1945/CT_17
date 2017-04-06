284c7a91323a320357a8caa85614da47c2004ba4
clif edwards <clif.edwards.2@gmail.com>
2017-04-06 10:32:55 -0400

    REFACT [src/EVOLVE_RCBounds() && test/EVOLVE_RCBounds()]

--- RC/test/EVOLVE_RCBounds.js
+++ RC/test/EVOLVE_RCBounds.js
@@ -16,47 +16,26 @@ let chai = require('chai')
     , expect = chai.expect
 ;
 
-let EVOLVE_this;
-let transforms;
-EVOLVE_this = (alterFn, dict) => R.evolve(alterFn, dict); // DICT -> DICT
+let EVOLVE_RCBounds = require('../src/EVOLVE_RCBounds');
 
-describe(`the Fn: EVOLVE_this( alterFn )(default_RCB) -> newRCB
-    
-    USAGE: EVOLVE_RCBounds = EVOLVE_this(WITH a partialed default_RCBounds argument) -> new_RCBounds
-    then EVOLVE_RCBounds( with an alterFn)  
+describe(`the Fn: EVOLVE_RCBounds IS R.evolve(R.__, WITH a hard coded Default_RCBounds argument)
+
+    GIVEN an DICT.alterFns IT WILL RETURN a new EVOLVED_RCBounds.  
     `, function () {
     beforeEach(function () {
-        this.default_RCB = {focus: {beg: 0, len: 2}};
-        this.CUT = EVOLVE_this(R.__, this.default_RCB);
+        this.STUB_alterFns = {focus: {beg: R.add(20)}};
     });
-    describe(`CONFIRM [default_RCB] the defaultReadClssBounds IS Valid.
-    `, function () {
-        it(`should be a DICTionary with at least 2 keys.
-    `, function () {
-            expect(this.default_RCB.focus).to.be.an('object').and.to.have.all.keys(['beg', 'len']);
-        });
-    });
-    describe(`CONFIRM EVOLVE_this is a function wanting a Transforms Fn.`, function () {
+    describe(`CONFIRM [EVOLVE_DICT is a function wanting a DICT.alterFns.]`, function () {
         it(`should be a function with one argument.    `, function () {
-            expect(this.CUT).to.be.a('function').and.to.have.lengthOf(1);
+            expect(EVOLVE_RCBounds).to.be.a('function').and.to.have.lengthOf(1);
         });
     });
-    describe(`CONFIRM EVOLVE_this(aTransforms) CAN alterFn the RCBounds DICT.    `, function () {
+    describe(`CONFIRM EVOLVE_DICT(STUB_alterFns) CAN alterFns the RCBounds DICT.    `, function () {
         it(`can return an altered RCBounds `, function () {
-            transforms = {focus: {beg: R.add(20)}};
-            expect(this.CUT(transforms).focus).to.be.an('object').and.to.have.all.keys(['beg', 'len']);
+            expect(EVOLVE_RCBounds(this.STUB_alterFns).focus).to.be.an('object').and.to.have.all.keys(['beg', 'len']);
         });
         it(`should see an altered key:value    `, function () {
-            transforms = {focus: {beg: R.always(20)}};
-            expect(this.CUT(transforms).focus.beg).to.equal(20);
-        });
-    });
-    describe(`CONFIRM EVOLVE_this(aTransforms W/O AN EXISTING Key) WILL NOT ALTER the RCBounds DICT.    `, function () {
-        it(`should see UNALTERED key:value    `, function () {
-            transforms = {focus: {WRONG: R.always(20)}};
-            //
-            expect(this.CUT(transforms).focus.beg).to.not.equal(20);
-            expect(this.CUT(transforms).focus.beg).to.equal(0);
+            expect(EVOLVE_RCBounds(this.STUB_alterFns).focus.beg).to.equal(20);
         });
     });
 });
