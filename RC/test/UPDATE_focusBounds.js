dda5eac8dd2f814be052263803069e1bf9192f1d
clif edwards <clif.edwards.2@gmail.com>
2017-04-12 09:10:14 -0400

    WIP STABLE [test/UPDATE_focusBounds()] CONDENSED Testing BY BUILDING a Pred Fn.

--- RC/test/UPDATE_focusBounds.js
+++ RC/test/UPDATE_focusBounds.js
@@ -6,9 +6,9 @@
 let R = require('ramda')
     , pipe = R.pipe
 //     , compose = R.compose
-//     , map = R.map
+    , map = R.map
     , curry = R.curry
-    , always = R.always
+    // , always = R.always
 ;
 
 let chai = require('chai')
@@ -55,28 +55,23 @@ const isCur = curry(
         let lteCurNdx1 = R.lte(R.__, dict.cur.len + f_ndx - 1);
         return R.both(gteCurNdx0, lteCurNdx1);
     });
-// const isThisCur = isCur(2);// N -> BOOL
 
-describe(`USE boolean functions: isOld, isCur, isNew TO ReadClass TRIAGE a cvNdx.
-        They DEPEND ON the curNdx0
+describe(`USE boolean functions: isOld, isCur, isNew TO TRIAGE a cvNdx in RC Space.
+        They DEPEND ON the curNdx
         `, () => {
-    let isThisCur;
+    let isThisCur, pred, STUB_cvNdxs, STUB_curNdx;
     beforeEach(function () {
         // loadFixtures('index.html'); //NOTE remember this breaks a mocha test
         // this.doc = document;
-        isThisCur = isCur(2);// N -> BOOL
+        STUB_cvNdxs = [0, 1, 2, 3, 4];
+        pred = pipe(isCur, map(R.__, STUB_cvNdxs)); //
     });
-
-    describe(`  CENTRAL CASE: USE Fn: isThisCur = isCur(cvNdx:2) TO begin triage of a cvNdx
+    describe(`  Fn: isCENTRAL CASE: curNdx > 0 AND curNdx < cvLen
     `, () => {
         it(`should define and triage aCV_Ndx ReadClss.`, () => {
-            isThisCur = isCur(2);// N -> BOOL
-            expect(isThisCur(0)).be.false
-            expect(isThisCur(1)).be.false
-            expect(isThisCur(2)).be.true
-            expect(isThisCur(3)).be.true
-            expect(isThisCur(4)).be.false
-        });
+            STUB_curNdx = 2;
+            expect(pred(STUB_curNdx)).to.eql([false, false, true, true, false])
+        })
     });
 });
 
