c4904793e016ad4dd1f1bb7ba89a5dcf0baf538e
clif edwards <clif.edwards.2@gmail.com>
2017-04-17 11:40:06 -0400

    WIP OK   [src/isRClss.js] and [test/isRClss.js] is_pstRCll()  IS WORKING; is_curRClss IS BROKEN; is_futRClss IS BROKEN

--- RC/src/is_RClss.js
+++ RC/src/is_RClss.js
@@ -12,7 +12,7 @@ let R = require('ramda')
 ;
 
 // CODE UNDER TEST
-// module.exports.cut = curry((cvLen, curLen, curBeg_Ndx) => {
+// module.exports.cut =
 
 let DEFINES_curEnd_Ndx = (cvLen, curLen, curBeg_Ndx) => {// (N.len,N.len,N.ndx,) -> N.ndx
     let sum = curLen + curBeg_Ndx;
@@ -20,9 +20,11 @@ let DEFINES_curEnd_Ndx = (cvLen, curLen, curBeg_Ndx) => {// (N.len,N.len,N.ndx,)
 };
 module.exports.DEFINES_curEnd_Ndx = DEFINES_curEnd_Ndx;
 
+// const _RClss = curry(
+// (cvLen, curLen, curBeg_Ndx) => {
 let gte_0 = curry(R.gte(R.__, 0));
 module.exports.gte_0 = gte_0;
-let lt_Beg = curry(beg => R.lt(R.__, beg));
+let lt_Beg = curry((beg, x) => x <= beg);
 module.exports.lt_Beg = lt_Beg;
 let gte_Beg = curry(beg => R.gte(R.__, beg));
 let lte_End = curry(end => R.lte(R.__, end));
@@ -30,10 +32,9 @@ let gt_End = curry(end => R.gt(R.__, end));
 let lte_cvLen = curry(cvl => R.lte(R.__, cvl));
 
 // the 3 is_xxxRClss Fns.
-let is_pstRClss = R.both(gte_0, lt_Beg);// -> N.cvNdx -> BOOL
-let is_curRClss = R.both(gte_Beg, lte_End);// -> N.cvNdx -> BOOL
-const is_futRClss = R.both(gt_End, lte_cvLen);// -> N.cvNdx -> BOOL
-
+let is_pstRClss = curry((beg, ndx) => gte_0(ndx) && lt_Beg(beg, ndx));// N.beg -> N.cvNdx -> BOOL
+let is_curRClss = curry((beg, end, ndx) => gte_Beg(beg, ndx) && lte_End(end, ndx));// -> N.cvNdx -> BOOL
+const is_futRClss = curry((end, cvl, ndx) => gt_End(end, ndx) && lte_cvLen(cvl, ndx));// -> N.cvNdx -> BOOL
 
 module.exports.is_pstRClss = is_pstRClss;
 module.exports.is_curRClss = is_curRClss;
--- RC/test/is_RClss_r.js
+++ RC/test/is_RClss_r.js
@@ -19,11 +19,12 @@ let chai = require('chai')
 
 
 let rc = require('../src/is_RClss')
-    , cut = rc.cut
+    // , cut = rc.cut
     , DEFINES_curEnd_Ndx = rc.DEFINES_curEnd_Ndx
-    , is_pstRClss = rc.is_pstRClss
-    , gte_0 = rc.gte_0
+    // , is_pstRClss = rc.is_pstRClss
+    // , gte_0 = rc.gte_0
 ;
+// let is= rc.RClss;
 
 context(`the three isRC's Fns: isPst(), isCur() and isFut() effectively define the rcWeights 
         and are used to weight the Style Attributes of an Element.  
@@ -43,7 +44,7 @@ context(`the beginning{curBeg} and the end{curEnd} define the three is_RClss bou
     describe(`Fn: DEFINE_curEnd() shortens the default curLen by 
         not allowing the curEnd to extend beyond the cvLength. `, () => {
         it(`should normally set curEnd === curBeg + curLen`, function () {
-            expect(curEnd_Ndx).to.be.equal(4).is.ok;//.and.to.be.equal(curBeg + curLen);
+            expect(curEnd_Ndx).to.be.equal(4).and.to.be.equal(curBeg + curLen);
         });
         it(`should however never set curEnd beyond the end of the cvLen.`, function () {
             expect(DEFINES_curEnd_Ndx(3, curLen, curBeg)).is.equal(3).and.is.equal(curBeg + 2).to.not.equal(curBeg + curLen);
@@ -51,32 +52,27 @@ context(`the beginning{curBeg} and the end{curEnd} define the three is_RClss bou
     });
 });
 context(`The is_xxxRClss DEFINE the RCWeightBounds.
-
 `, () => {
-    xdescribe(`Fn: is_pstClss( cvNdx ) IS gte 0 && lt curBeg`, () => {
-        let curBeg, curLen, curEnd, cvLen, lt_curBeg, gte_0;
+    describe(`Fn: is_pstClss( cvNdx ) IS gte 0 && lt curBeg`, () => {
+        let cvLen, curBeg, curLen, is_pstClss;
         beforeEach(function () {
             cvLen = 5;
             curLen = 3;
             curBeg = 1;
-            this.cut = rc.cut(cvLen, curLen, curBeg);
         });
-        it(`should cut ....`, () => {
-            (this.cut.is_pstRClss(0)).should.equal(true);// FIX BROKEN >>>
+        it(`should be true:: individual tests:  cvNdx:0 is gte curBeg:1 && cvNdx:0 is lte curEnd:3.`, () => {
+            expect(rc.gte_0(0)).to.be.true; // 0 gte 0
+            expect(rc.lt_Beg(curBeg, 0)).to.be.true;// 0 lt  1
         });
-        //
-        // it(`should be true for SIMPLE a cvNdx that is gte 0 && lte curBeg.`, () => {
-        //     expect(rc.gte_0(0)).to.be.true; // 0 gte 0
-        //     expect(R.lt(0, curBeg)).to.be.true;// 0 lt  1
-        //    expect(is_pstRClss(0)).to.be.true;
-        // });
-        it(`should be false for a cvNdx that is gte curBeg BUT NOT lt curBeg.`, () => {
+        it(`should be true::is_pstClss(cvNdx:0) is gte 0 && lte curEnd:3.`, () => {
+            expect(rc.is_pstRClss(curBeg, 0)).to.be.true;
+        });
+        xit(`should be false for a cvNdx that is gte curBeg BUT NOT lt curBeg.`, () => {
             // expect(is_pstRClss(1)).to.be.false
         });
     });
     xdescribe(`Fn: is_curClss( cvNdx ) IS gte curBeg && lte curEnd..`, () => {
         let curBeg, curLen, curEnd, cvLen, gte_curBeg, lte_curEnd;
-        // let curBeg, curLen, curEnd, cvLen, lte_curEnd;
         beforeEach(function () {
             cvLen = 5;
             curLen = 3;
@@ -85,7 +81,7 @@ context(`The is_xxxRClss DEFINE the RCWeightBounds.
             curEnd = DEFINES_curEnd_Ndx(cvLen, curLen, curBeg);
             lte_curEnd = lte_End(curEnd);
         });
-        it(`should be true for SIMPLE a cvNdx that is gte curBeg && lte curEnd.`, () => {
+        it(`should be true for SIMPLE cvNdx:1 that is gte curBeg:1 && lte curEnd:3.`, () => {
             expect(gte_curBeg(1)).to.be.true;
             expect(lte_curEnd(1)).to.be.true;
             // expect(is_curRClss(1)).to.be.true;
