c674b59b49d9de3223a073128e27a27b08ffdb42
clif edwards <clif.edwards.2@gmail.com>
2017-05-05 12:17:36 -0400

    new WIP [ STYLE/src/EVOLVE_Style.js ] STUB  FOR NOW

--- STYLE/src/EVOLVE_Style.js
+++ STYLE/src/EVOLVE_Style.js
@@ -2,3 +2,19 @@
  * EVOLVE_Style.js
  */
 "use strict";
+
+let R = require('ramda')
+// , pipe = R.pipe
+//     , compose = R.compose
+//     , map = R.map
+    , curry = R.curry
+;
+
+let EVOLVE_Style = curry(
+    () => {
+        let dflt = require('.Dflt_Style')
+        //STUB
+        return dflt
+    }
+);
+module.exports.EVOLVE_Style = EVOLVE_Style;
