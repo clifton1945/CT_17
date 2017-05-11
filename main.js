dcad587bae9ddd6041b2dda936d7291bba5e1e7f
clif edwards <clif.edwards.2@gmail.com>
2017-05-11 07:38:32 -0400

    STABLE  [main.js ] REFACT names and format

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 "use strict";
-let C = require('./h/C_in_');
-let _inConsole = C._inConsole;
-
+let C = require('./h/C_in_')
+    , _inConsole = C._inConsole
+;
 let R = require('ramda')
-    // , curry = R.curry
+    , curry = R.curry
     // , always = R.always
     // , map = R.map
     // , pipe = R.pipe
@@ -12,56 +12,62 @@ let R = require('ramda')
 let TRK = "wbSample/main.js";
 _inConsole("< IN  " + TRK);
 
-// get the CVList of Verses
+// ...................... get the CVList of Chapter Verses
 /**
  *  _CVList:: Fn(DOC -> LIST)
  */
-let _CVList = require('./CV/src/SELECT_ChptVerses')._CVList; //  Fn(DOC -> LIST)
-// let CVList = _CVList(document);
+let _CVList = require('./CV/src/SELECT_ChptVerses')._CVList
+; //  Fn(DOC -> LIST)
 
-
-// BUILD an new CSD
+// ..................... BUILD a new Verse Property CSD
 /**
  *  ..... EVOLVE_CSD:: ( OBJ_trnsfrms -> CSD_new )
  * @param OBJ_trnsfrms
  * @return CSD_new
  */
-let EVOLVE_CSD = require('./STYLE/src/EVOLVE_Style').EVOLVE_CSD; //  Fn( OBJ_trnsfrms -> CSD_new )
+let EVOLVE_CSD = require('./STYLE/src/EVOLVE_Style').EVOLVE_CSD
+; //  Fn( OBJ_trnsfrms -> CSD_new )
 
-// now the CSD_OBJ_trnsfrms
+// ..................... now the CSD_OBJ_trnsfrms
 /**
- *
+ *  ..... CSD_OBJ_trnsfrms
  * @type {{backgroundColor: *, opacity: *, fontSize: *}}
  */
 let CSD_OBJ_trnsfrms = {
-    backgroundColor: R.always('green')
+        backgroundColor: R.always('lavender')
     , opacity: R.always('0.50')
-    , fontSize: R.always('70%')
-};
+    // , fontSize: R.always('70%')
+    }
+; // -> DICT.styleTransforms
+
+// ..................... UPDATE Elements
 /**
  * ..... UPDATE_ElemStyle:: OBJ.propertyCSD -> ( ELEM.elem -> ELEM.style.propertyCSD )
  * @param propertyCSD
  * @param elem
  * @return Fn:  ELEM.style.propertyCS
  */
-let UPDATE_ElemStyle = require('./CV/src/main_UPDATE_anElement').UPDATE_ElemStyle; //  OBJ.propertyCSD -> ( ELEM.elem -> ELEM.style.propertyCSD )
-
+let UPDATE_ElemStyle = require('./CV/src/main_UPDATE_anElement').UPDATE_ElemStyle
+; //  OBJ.propertyCSD -> ( ELEM.elem -> ELEM.style.propertyCSD )
 /**
- * ..... UPDATE_Elem::  ( ELEM.elem -> ELEM.elem w/ elem.style.propertyCSD )
+ * ..... UPDATE_anElem::  ( ELEM.elem -> ELEM.elem w/ elem.style.propertyCSD )
  * @param elem
  * @return Elem w/new propertyObject
  */
-let UPDATE_Elem = R.pipe(EVOLVE_CSD, UPDATE_ElemStyle)(CSD_OBJ_trnsfrms); // Fn( ELEM.elem -> ELEM.elem w/ elem.style.propertyCSD )
+let UPDATE_anElem = R.pipe(EVOLVE_CSD, UPDATE_ElemStyle)(CSD_OBJ_trnsfrms)
+; // Fn( ELEM.elem -> ELEM.style.propertyCSD )
 
-
+const UPDATE_allElems =
 R.addIndex(R.map)(
     (el, ndx, lst) => {
-        UPDATE_Elem(el)
+        UPDATE_anElem(el)
     })
 (_CVList(document))
-;
+;// Functor f => (a -> b ) -> f a -> f b
 
 let init_keyActions = require('./CV/src/main_keyActions').init_keyActions;
+
+
 document.addEventListener("keydown", init_keyActions(8), false);
 
 
