20349a4530c07619ce86eb80654c9a60291a5f19
clif edwards <clif.edwards.2@gmail.com>
2017-05-19 15:25:01 -0400

    CREATED [ RScp/src/RSpc_Lengths.js ]  STABLE w/ tests

---
+++ RSpc/src/RSpace_Lengths.js
@@ -0,0 +1,28 @@
+/**
+ *RSpace_Lengths
+ */
+"use strict";
+let R = require('ramda')
+    , curry = R.curry
+    , always = R.always
+;
+
+let RSpace_Lengths = curry(
+    /**
+     *
+     * @param cspc_len
+     * @param cspc_focus
+     * @return RSpc_Lengths Obj/Dict{*}
+     */
+    (cspc_len, cspc_focus) => {
+        let dflt = {pst: 0, cur: 0, fut: 0};
+        let trnsfrms = {
+            pst: (cspc_focus < 0 || cspc_focus >= cspc_len) ? 0 : always(cspc_focus),
+            cur: (cspc_focus < 0 ) ? 0 : always(1),
+            fut: (cspc_focus > (cspc_len - 1)) ? 0 : always(cspc_len - cspc_focus - 1)
+        }
+        return R.evolve(trnsfrms, dflt);
+    }
+);
+
+module.exports = RSpace_Lengths;
--- RSpc/test/RSpace_Lengths_test.js
+++ RSpc/test/RSpace_Lengths_test.js
@@ -1,50 +1,31 @@
+
 /**
  *RSpace_Lengths
  */
 "use strict";
+
+
 let R = require('ramda')
-    , curry = R.curry
+    // , curry = R.curry
     , always = R.always
 ;
 let context = describe
 ;
 let chai = require('chai')
-    // , should = chai.should()
     , expect = chai.expect
+    // , should = chai.should()
 ;
 
-// let RSpace_Lengths = require('../src/RSpace_Lengths');
-let RSpace_Lengths = curry(
-    /**
-     *
-     * @param cspc_len
-     * @param cspc_focus
-     * @return {*}
-     */
-    (cspc_len, cspc_focus) => {
-        let dflt = {pst: 0, cur: 0, fut: 0};
-        let trnsfrms = {
-            pst: (cspc_focus < 0 || cspc_focus >= cspc_len) ? 0 : always(cspc_focus),
-            cur: (cspc_focus < 0 ) ? 0 : always(1),
-            fut: (cspc_focus > (cspc_len - 1)) ? 0 : always(cspc_len - cspc_focus - 1)
-        }
-        return R.evolve(trnsfrms, dflt);
-    }
-);
-
+let RSpace_Lengths = require('../src/RSpace_Lengths');
 
 context(`An Element's ReadSpaceObject {} ->> Lengths ARE DETERMINED BY <<- (the chptFocusNdx && chptArrayLength).
      { pstLen: N||0, curLen:N, fur:N||0 }
-    
-    NOTE 2017.04.28 I have dropped the currentRC_ Len for a while. The curLen || lenCUR is always 1.
     `, () => {
 
     describe(`RSpcLens:: N.CSpc_Len -> (N.CSpc_Focus -> {{},{}, {}})`, () => {
-        let dflt, CUT, CSpcFocus, CSpcLen;
+        let dflt, CSpcLen;
         beforeEach(function () {
             dflt = {pst: 0, cur: 0, fut: 0};
-            // CSpcFocus = 1;
-            // CSpcLen = R.length(0, 1, 2, 3, 4);
         });
         it(`should usually have 3 lengths { pst:n, cur:1, fut:n } for focusNdx > 1 && focusNdx < CSpcLen`, () => {
             expect(RSpace_Lengths(5, 1)).to.deep.equal({pst: 1, cur: 1, fut: 3});
