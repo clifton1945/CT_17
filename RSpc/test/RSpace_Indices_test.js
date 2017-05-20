80cd64585ad9cfe5387979605aa08385090b1b24
clif edwards <clif.edwards.2@gmail.com>
2017-05-20 08:45:43 -0400

    REFACTusing an arrayinput and a pipe config [ RScp/src/RSpace_SizeObj.js ]  STABLE w/ tests

--- RSpc/test/RSpace_Indices_test.js
+++ RSpc/test/RSpace_Indices_test.js
@@ -5,8 +5,9 @@
 
 
 let R = require('ramda')
-    // , curry = R.curry
-    , always = R.always
+    , curry = R.curry
+    , pipe = R.pipe
+    // , always = R.always
 ;
 let context = describe
 ;
@@ -16,32 +17,35 @@ let chai = require('chai')
 ;
 
 
-let RSpace_Lengths = require('../src/RSpace_Lengths');
-// CSpc_Length -> ( CSpc_Focus -> RSpc_Lengths )
+let RSpaceSize_Obj = require('../src/RSpace_Sizes').RSpaceSize_Obj;
+// :: CSpc_Array -> CSpc_Focus -> RSpc_Size_Obj
 
-context(`An Element's ReadSpace Indices RETURNS an RSpc_Lengths Obj 
-    ->> { pstLen: N||0, curLen:N, fur:N||0 }
-    
-    AS DETERMINED BY <<- (CSpace_Lengths && the CSpace Index).
-    
-    `, () => {
+const rSpcNdx = curry(function (_Obj) {
 
-    describe(`RSpcLens:: N.CSpc_Len -> (N.CSpc_Focus -> {{},{}, {}})`, () => {
-        let dflt, CSpcLen;
+});
+
+context(`An Element's RSpace_Indices RETURNS its RSpc_Index GIVEN its CSpc_Index.
+    `, function () {
+
+    describe(`rSpcNdx:: rSpcObj -> ( cSpcNdx -> rSpcNdx )
+        `, function () {
+        let dflt, _Obj, _CUT, Focus;
         beforeEach(function () {
             dflt = {pst: 0, cur: 0, fut: 0};
+            _Obj = RSpaceSize_Obj([0, 1, 2, 3, 4]);
+            // _CUT = pipe();
         });
-        it.skip(`should usually have 3 lengths { pst:n, cur:1, fut:n } for focusNdx > 1 && focusNdx < CSpcLen`, () => {
-            expect(RSpace_Lengths(5, 1)).to.deep.equal({pst: 1, cur: 1, fut: 3});
+        it.only(`expects a well formed RSpcSize_Obj as an Argument `, () => {
+            expect(_Obj(1)).to.deep.equal({pst: 1, cur: 1, fut: 3});
         });
-        it.skip(`should have 2 lengths { pst:n, cur:1, fut:0 } IF focusNdx > 1 && focusNdx == CSpcLen`, () => {
+        it(`expects to returns a Num`, () => {
             expect(RSpace_Lengths(5, 0)).to.deep.equal({pst: 0, cur: 1, fut: 4});
         });
-        it.skip(`should usually have 1 lengths { pst:0, cur:1, fut:0} for focusNdx == 0 && CSpcLen == 1`, () => {
-            expect(RSpace_Lengths(1, 0)).to.deep.equal({pst: 0, cur: 1, fut: 0});
-        });
-        it.skip(`should usually have 0 lengths { pst:0, cur:0, fut:0} for focusNdx == 0 && focusNdx == CSpcLen`, () => {
-            expect(RSpace_Lengths(2, 77)).to.deep.equal({pst: 0, cur: 1, fut: 0});
-        });
+        // it.skip(`should usually have 1 lengths { pst:0, cur:1, fut:0} for focusNdx == 0 && CSpcLen == 1`, () => {
+        //     expect(RSpace_Lengths(1, 0)).to.deep.equal({pst: 0, cur: 1, fut: 0});
+        // });
+        // it.skip(`should usually have 0 lengths { pst:0, cur:0, fut:0} for focusNdx == 0 && focusNdx == CSpcLen`, () => {
+        //     expect(RSpace_Lengths(2, 77)).to.deep.equal({pst: 0, cur: 1, fut: 0});
+        // });
     });
 });
