42a0212f68e78c7b6c1ffaa32afe6006ba0f2b58
clif edwards <clif.edwards.2@gmail.com>
2017-05-26 10:01:07 -0400

    RScp/src/strColor_From_ndxInRSpc.js && .../test/] WIP first try

---
+++ RSpc/src/strColor_From_ndxInRSpc.js
@@ -0,0 +1,29 @@
+/**
+ *RSpace_Indices
+ */
+"use strict";
+let R = require('ramda')
+    , curry = R.curry
+    // , pipe = R.pipe
+    // , always = R.always
+;
+const strColor = curry(
+    /**
+     *  Fn: strColor  RETURNS -> strInRSpc, <- FROM ndxInRSpc
+     *
+     * @param str:  RSpc Size Object: e.g.{pst: 2, cur: 1, fut: 2}
+     * @param ndx:  Element Index in CSpc
+     * @return {number} : this Element Index in RSpc
+     *
+     *  NOTE: this is a special case because the current ReadSpace Size is always just 1.
+     */
+    function (str, ndx) {
+        let pst = str.pst;
+        return (ndx < 0) ? -9999 :
+            (ndx < pst) ? ndx :
+                (ndx === pst) ? 0 :
+                    (ndx < (pst + 1 + str.fut)) ? ndx - pst - 1 :
+                        9999
+    }); // OBJ.rSpcSizes -> (N.cSpcNdx -> N.rSpcNdx
+
+module.exports = strColor;
---
+++ RSpc/test/strColor_From_ndxInRSpc_test.js
@@ -0,0 +1,63 @@
+/**
+ *RSpace_Indices
+ */
+"use strict";
+
+
+// let R = require('ramda')
+// , curry = R.curry
+// , pipe = R.pipe
+// , always = R.always
+// ;
+let context = describe
+;
+let chai = require('chai')
+    , expect = chai.expect
+    // , should = chai.should()
+;
+
+let RSpace_SizeObj = require('../src/RSpace_Sizes').RSpace_SizeObj; // CSpc_Arr -> ( CSpc_FocusN -> RSpc_LengthsOBJ)
+let RSpcNdx = require('../src/RSpace_Indices'); // OBJ.rSpcSizes -> ( N.cSpcNdx -> N.RSpcNdx )
+
+context(`An Element's RSpace_Indices::
+    RETURNS its RSpc_Index 
+    GIVEN its CSpc_Index.
+    `, function () {
+
+    describe(`RSpcNdx:: rSpcObj -> ( cSpcNdx -> RSpcNdx )
+        `, function () {
+        let _Focus, _Obj, _CUT;
+        beforeEach(function () {
+            _Obj = RSpace_SizeObj([0, 1, 2, 3, 4]);
+            _Focus = 2;
+            _CUT = RSpcNdx(_Obj(_Focus)); //  {pst: 2, cur: 1, fut: 2}
+        });
+        it(`expects a well formed RSpcSize_Obj as an Argument `, () => {
+            expect(_Obj(1)).is.deep.equal({pst: 1, cur: 1, fut: 3});
+        });
+        it(`expects TO RETURN a Num`, () => {
+            expect(_CUT(555)).is.a('number');
+        });
+        it(`expects a CSpcNdx argument <  CSpcFocus TO RETURN CSpcNdx`, () => {
+            expect(_CUT(1)).is.equal(1);
+            expect(_CUT(3)).is.not.equal(3);
+        });
+        it(`expects a CSpcNdx argument == CSpcFocus TO RETURN 0`, () => {
+            expect(_CUT(1)).is.equal(1);
+            expect(_CUT(2)).is.equal(0);
+            expect(_CUT(3)).is.not.equal(3);
+        });
+        it(`expects a CSpcNdx argument >  CSpcFocus TO RETURN CSpcNdx - pst Siz - cur Siz. `, () => {
+            expect(_CUT(0)).is.equal(0);
+            expect(_CUT(1)).is.equal(1);
+            expect(_CUT(2)).is.equal(0);
+            expect(_CUT(3)).is.equal(0);
+            expect(_CUT(4)).is.equal(1);
+        });
+        it(`expects TO NOT HAVE BOUNDARY conditions Problems; i.e. a CSpcNdx < 0 OR CSpcNdx >= CSpcArrSize.
+            GOOD LUCK ON THAT SO a first step fail indicator is RETURNING -9999 & 9999 `, () => {
+            expect(_CUT(-1)).is.equal(-9999);
+            expect(_CUT(5)).is.equal(9999); // 5 IS the Size of the Array
+        });
+    });
+});
