81db0eb8f55f1c053dc1cd6aac751e9fe02351cd
clif edwards <clif.edwards.2@gmail.com>
2017-05-29 07:09:35 -0400

    BEGIN to use readme as my begin/end workday prompts

--- RSpc/src/revise_ndxRSpc.js
+++ RSpc/src/revise_ndxRSpc.js
@@ -11,14 +11,14 @@ let R = require('ramda')
 const revise_ndxRSpc = curry(
     /**
      * Fn .... revise_ndxRSpc::  DCT -> ( N -> N )
-     *
-     * this function TRANSFORMS a CSpc index INO a RScp index.
-     *  it REQUIRES, in addition to CSpc index, the current RSpc SizesDCT
-     *      which typically is partialled.
-     *
      * @param dct:  sizesDct in RSpc: e.g.{pst: 2, cur: 1, fut: 2}
      * @param ndx:  an Element's Index in CSpc: e.g. 3
      * @return {number} : this Elements Index in RSpc: e.g. 0
+     *
+     * this function TRANSFORMS a CSpc index INTO a RScp index.
+     *  it REQUIRES, in addition to CSpc index, the current RSpc SizesDCT
+     *      which typically is partialled.
+     *
      *
      * // USAGE:
      * let revise = require('...path/revise_ndxRSpc');//  DCT -> ( N -> N )
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+* 170529
+    adding USAGE really helps me be able to see the big picture.
+    NOW:    do I need to use a RSpc clssName BEFORE all the STYLES 
+    TODO:  BUILD a testAll test
+    TODO:  RENAME wbSample/STYLE -> SSpc OR StylSpc || ???
