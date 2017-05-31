e2203e6835c43d945aa9116e89dcc3735f23dbd0
clif edwards <clif.edwards.2@gmail.com>
2017-05-31 08:57:39 -0400

    [ TRNSFRM_Attr ]  REFACT Names && DOC mostly

--- SSpc/src/TRNSFRM_Attr.js
+++ SSpc/src/TRNSFRM_Attr.js
@@ -10,8 +10,8 @@ let R = require('ramda')
 ;
 let Attr = R.curry(
     /**
-     *  ..... TRNSFRM_Attr:: STR.key -> ( STR.val -> DICT.csd )
-     * RETURNS a transformer Fn to evolve an element.style.
+     *  ..... TRNSFRM_Attr[arity:2] STR.key -> ( STR.val -> DICT.csd )
+     * arity:2 RETURNS a transformer Fn to evolve an element.style.
      * USAGE:
      *  let Trnsfrms = require('../src/TRNSFRM_Attr')
      *  let TRNSFRM_Attr = Trnsfrms.Attr; // STR.key -> ( STR.val -> DCT.CSD )
@@ -21,7 +21,8 @@ let Attr = R.curry(
      */
     (key, val) => {
         return {[key]: R.always(val)}
-    });
-module.exports.Attr = Attr; // ( STR.key -> STR.color -> Fn.TRNSFRM_Key )
-module.exports.bgColor = Attr('backgroundColor'); // ( STR.Valu -> Fn.TRNSFRM_backgroundColor )
-module.exports.opacity = Attr('opacity'); // ( STR.Valu -> Fn.TRNSFRM_backgroundColor )
+    }
+);
+module.exports.Attr = Attr; // ( STR.key -> STR.valu -> Fn.TRNSFRM_Key )
+module.exports.bgColor = Attr('backgroundColor'); // ( STR.color -> Fn.TRNSFRM_backgroundColor )
+module.exports.opacity = Attr('opacity'); // ( STR.valu -> Fn.TRNSFRM_backgroundColor )
