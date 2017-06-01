dd8226c588f86ecd61e524bb4d3b8849e3458578
clif edwards <clif.edwards.2@gmail.com>
2017-06-01 10:55:08 -0400

    [ RESIZE_Rspc ]  STABLE with RESIZE_fromFocus

--- RSpc/src/RESIZE_Rspc.js
+++ RSpc/src/RESIZE_Rspc.js
@@ -9,22 +9,18 @@ let R = require('ramda')
 
 const RESIZE_ = curry(
     /**
-     *  Fn...... RESIZE_:: N|ARR -> ( N -> DICT )
-     *
-     *  RESIZE_: arity:2 function RETURNS a DICT of RClass sizes
-     *      GIVEN a ChptSize
-     *      GIVEN a ndxFocus
-     *
-     * @param size_chpr : N
-     * @param ndx_focus : N
-     * @return {*}: an evolved SizesObj : DICT.
+     * Fn...... RESIZE_:: is the base function to return a DICT of RClass Sizes: {pst:N, cur:1, fut:N}
+     * @sig:  N|ARR -> ( N -> DICT )
+     * @param size_chpr in Chptr Space
+     * @param ndx_focus in Chptr Space
+     * @return {*}: an evolved SizesObj DICT:  {pst:N, cur:1, fut:N}.
      *
      * USAGE:
-     * let RESIZE_ = require('...path/RESIZE_').fromSize(5);
+     * let RESIZE_fromSize = require('...path/RESIZE_').fromSize(5);
      * // OR
-     * let RESIZE_ = require('...path/RESIZE_').fromArray([0,1,2,3,4]);
-     * // THEN if needed,
-     * let EVOLVED_SizesDict = RESIZE_(3): //-> {pst: 3, cur: 1, fut: 1}
+     * let RESIZE_fromArray = require('...path/RESIZE_').fromArray([0,1,2,3,4]);
+     * // OR
+     * let RESIZE_fromfromFocus = require('...path/RESIZE_').fromFocus(2);
      */
     (size_chpr, ndx_focus) => { // N|ARR -> N -> DICT
         let dflt = {pst: 0, cur: 0, fut: 0}; // NOTE: I am embedding this constant. Shouldn't I retrieve It ? ??
@@ -39,6 +35,8 @@ const RESIZE_ = curry(
 
 const fromSize = RESIZE_; // this is the default fromSize
 const fromArray = R.pipe(R.length, RESIZE_);
+const fromFocus = R.flip(RESIZE_);
 
 module.exports.fromSize = fromSize;
 module.exports.fromArray = fromArray;
+module.exports.fromFocus = fromFocus;
--- RSpc/test/RESIZE_Rspc_test.js
+++ RSpc/test/RESIZE_Rspc_test.js
@@ -13,11 +13,10 @@ let context = describe
 let chai = require('chai')
     , expect = chai.expect
 ;
-context(`RESIZE_: Using different arrangements of arity:2 params fromChptSpace, returns Functions: RESIZE_fromSize, RESIZE_fromArray, RESIZE_fromFocus
+let iSuite = context(`RESIZE_: Using different arrangements of arity:2 params fromChptSpace, returns Functions: RESIZE_fromSize, RESIZE_fromArray, RESIZE_fromFocus
         `, function () {
 
-    describe(`RESIZE_fromSize  returns Fn (  ndx_focus -> RESIZE_fromSize )  given partial: size_chpr
-    `, function () {
+    describe(`RESIZE_fromSize  returns Fn (  ndx_focus -> RESIZE_fromSize )     given partial: size_chpr. `, function () {
 
         let RESIZE_fromSize = require('../src/RESIZE_Rspc').fromSize; // size_chpr -> (  ndx_focus -> RESIZE_in_RSpc )
 
@@ -35,26 +34,59 @@ context(`RESIZE_: Using different arrangements of arity:2 params fromChptSpace, 
         it(`should RESIZE RspcSizes given a fixed CspcSize: 1`, () => {
             expect(RESIZE_fromSize(1, 0)).is.deep.equal({pst: 0, cur: 1, fut: 0});
         });
-        it(`RESIZE RspcSizes given a fixed CspcSize: 0 -> IS ALWAYS {pst: 0, cur: 1, fut: 0}`, () => {
+        it(`should RESIZE RspcSizes given a fixed CspcSize: 0 -> IS ALWAYS {pst: 0, cur: 1, fut: 0}`, () => {
             expect(RESIZE_fromSize(0, 0)).is.deep.equal({pst: 0, cur: 1, fut: 0});
             expect(RESIZE_fromSize(0, 77)).is.deep.equal({pst: 0, cur: 1, fut: 0});
         });
     });
-    let RESIZE_fromArray = require('../src/RESIZE_Rspc').fromArray; // chptArray -> (  ndx_focus -> RESIZE_in_RSpc )
-    describe(`RESIZE_fromArray:: RETURNS Fn (CSpc_FocusNdx -> RSpc_SizesOBJ)
-        FROM Arg: CSpc_VersArr
-        note: THIS IS A PREFERRED CONFIG that uses R.pipe`, function () {
-        let CUT, ChptArray;
+    describe(`RESIZE_fromArray:: returns Fn (  ndx_focus -> RESIZE_fromArray )  given partial: array_chpr. `, function () {
+
+        let RESIZE_fromArray = require('../src/RESIZE_Rspc').fromArray; // chptArray -> (  ndx_focus -> RESIZE_in_RSpc )
+
+        let _CUT;
         beforeEach(function () {
-            CUT = RESIZE_fromArray([0, 1, 2, 3, 4]); //STUB
+            _CUT = RESIZE_fromArray([0, 1, 2, 3, 4]); //STUB
         });
-        it(`should have 3 lengths { pst:n, cur:1, fut:n } GIVEN focusNdx > 1 && focusNdx < ChptArray`, () => {
-            CUT = RESIZE_fromArray([0, 1, 2, 3, 4]); //STUB
-            expect(CUT(3)).is.deep.equal({pst: 3, cur: 1, fut: 1});
+        it(`should RESIZE RspcSizes given a fixed CspcArray: [0, 1, 2, 3, 4]`, () => {
+            _CUT = RESIZE_fromArray([0, 1, 2, 3, 4]); //STUB
+            expect(_CUT(3)).is.deep.equal({pst: 3, cur: 1, fut: 1});
         });
-        it(`should usually have 0 lengths { pst:0, cur:0, fut:0} for focusNdx == 0 && focusNdx == ChptArray`, () => {
-            CUT = RESIZE_fromArray([]); //STUB
-            expect(CUT(0)).is.deep.equal({pst: 0, cur: 1, fut: 0});
+        it(`should RESIZE RspcSizes given a fixed CspcArray: [] -> IS ALWAYS {pst: 0, cur: 1, fut: 0}`, () => {
+            _CUT = RESIZE_fromArray([]); //STUB
+            expect(_CUT(0)).is.deep.equal({pst: 0, cur: 1, fut: 0});
         });
-    })
+    });
+    describe(`RESIZE_fromFocus: 3  returns Fn (  size_chpr -> RESIZE_fromFocus )  given partial: ndx_focus. `, function () {
+
+        let RESIZE_fromFocus = require('../src/RESIZE_Rspc').fromFocus; // ndx_focus -> ( chptArray -> RESIZE_in_RSpc )
+        let _CUT;
+        beforeEach(function () {
+            let focusNdx = 3;
+            _CUT = RESIZE_fromFocus(focusNdx); // now expects a chpt Size
+        });
+        it(`should RESIZE RspcSizes given a fixed focusNdx: 3 `, () => {
+            expect(_CUT(3)).is.not.deep.equal({pst: 3, cur: 1, fut: 0}); // OOPS, this returns default 0,1,0
+            expect(_CUT(4)).is.deep.equal({pst: 3, cur: 1, fut: 0});
+            expect(_CUT(5)).is.deep.equal({pst: 3, cur: 1, fut: 1});
+            expect(_CUT(6)).is.deep.equal({pst: 3, cur: 1, fut: 2});
+        });
+    });
+    describe(`RESIZE_fromFocus: 1  returns Fn (  size_chpr -> RESIZE_fromFocus )  given partial: ndx_focus. `, function () {
+
+        let RESIZE_fromFocus = require('../src/RESIZE_Rspc').fromFocus; // ndx_focus -> ( chptArray -> RESIZE_in_RSpc )
+        let _CUT;
+        beforeEach(function () {
+            let focusNdx = 1;
+            _CUT = RESIZE_fromFocus(focusNdx); // now expects a chpt Size
+        });
+
+        it(`should RESIZE RspcSizes given a fixed focusNdx: 1 which is less than the ChptSize. `, () => {
+            // expect(_CUT(3)).is.deep.equal({pst: 1, cur: 1, fut: 1});
+            expect(_CUT(2)).is.deep.equal({pst: 1, cur: 1, fut: 0});
+            expect(_CUT(3)).is.deep.equal({pst: 1, cur: 1, fut: 1});
+            expect(_CUT(4)).is.deep.equal({pst: 1, cur: 1, fut: 2});
+            expect(_CUT(5)).is.deep.equal({pst: 1, cur: 1, fut: 3});
+            expect(_CUT(6)).is.deep.equal({pst: 1, cur: 1, fut: 4});
+        });
+    });
 });
