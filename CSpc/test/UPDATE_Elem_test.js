0ce804bd013dd80208ffa8566936ec0481200026
clif edwards <clif.edwards.2@gmail.com>
2017-06-08 09:37:58 -0400

    All Three Mocha Tests ..__inCSpc, __RSpc and SSpc test STABLE.

--- CSpc/test/UPDATE_Elem_test.js
+++ CSpc/test/UPDATE_Elem_test.js
@@ -9,8 +9,8 @@ let chai = require('chai')
     , should = chai.should()
     // , expect = chai.expect
 ;
-context(` Fn: UPDATE_ anElem applies each style CSD property to Elem's style object. 
-    @ symb: ( {ELEM} ) {UPDATE_anElem(DICT.CSD)} ->  {ELEM}       
+context(` Fn: UPDATE_ byElem applies each style CSD property to Elem's style object. 
+    @ symb: ( {ELEM} ) {UPDATE_byElem(DICT.CSD)} ->  {ELEM}       
      `, function () {
 });
 describe(`{UPDATE_} Fn:(DICT.CSD)(ELEM) {UPDATE_} -> ELEM
@@ -26,24 +26,24 @@ describe(`{UPDATE_} Fn:(DICT.CSD)(ELEM) {UPDATE_} -> ELEM
         UPDATE_.should.be.a('Function').and.is.length(2);
     });
 });
-describe(`{UPDATE_anElem} Fn:( ELEM) {UPDATE_anElem(DICT.CSD)} ->  ELEM 
+describe(`{UPDATE_byElem} Fn:( ELEM) {UPDATE_byElem(DICT.CSD)} ->  ELEM 
         `, function () {
 
-    let UPDATE_anElem = require('../src/UPDATE_Elem').anElem({"opacity": "0.5", "bgColor": "green"});
+    let UPDATE_byElem = require('../src/UPDATE_Elem').byElem({"opacity": "0.5", "bgColor": "green"});
     beforeEach(function () {
         this.STUB_CSD = {"opacity": "0.5", "bgColor": "green"};
         this.STUB_Elem = {style: {opacity: 1, bgColor: ''}};
     });
     it(`should.be a Fn of arity:1 and Expect an Element.`, () => {
-        UPDATE_anElem.should.be.a('Function').and.is.length(1);
+        UPDATE_byElem.should.be.a('Function').and.is.length(1);
     });
     it(`should.produce a new Elem given an Element: .`, function () {
-        UPDATE_anElem(this.STUB_Elem).should.be.a('Object')
+        UPDATE_byElem(this.STUB_Elem).should.be.a('Object')
             .and.have.property('style')
-            .and.deep.equal({"opacity": "0.5", "bgColor": "green"});
+            .and.is.deep.equal({"opacity": "0.5", "bgColor": "green"});
     });
 });
-describe(`{UPDATE_byCSD} Fn:( DICT.CSD) {UPDATE_anElem( ELEM)}  -> ELEM
+describe(`{UPDATE_byCSD} Fn:( DICT.CSD) {UPDATE_byElem( ELEM)}  -> ELEM
         `, function () {
 
     let UPDATE_byCSD = require('../src/UPDATE_Elem').byCSD({style: {opacity: 1, bgColor: ''}});
--- CSpc/test/_ELEM_by_test.js
+++ CSpc/test/_ELEM_by_test.js
@@ -14,32 +14,55 @@ context(` Fn: _ELEM anElem applies each style CSD property to Elem's style objec
     @ symb: ( {ELEM } ) {_ELEManElem(DICT.CSD)} ->  {ELEM }       
      `, function () {
 });
-describe(`{_ELEM_byTrnsfrm}::  (ELEM ) {Fn:_ELEM((CSD))} -> ELEM 
+describe(`{_ELEM_byTrnsfrm}::  (ELEM ) { _ELEM_byTrnsfrm( STUB_CSD) } -> ELEM 
         `, function () {
 
     //      (ELEM) {Fn:_ELEM(Fn:(Trnsfrm))} -> ELEM
     let _ELEM_byTrnsfrm = require('../src/UPDATE_Elem')._byTrnsfrm;
 
     beforeEach(function () {
+        // this.STUB_CSD = {"opacity": "0.5", "backgroundColor": "green"};
+        // this.STUB_TRNSFRMR = {
+        //     backgroundColor: R.always('yellow'),
+        //     opacity: R.always('0.5')
+        // };
+        this.STUB_Elem = {style: {opacity: 1, backgroundColor: ''}};
+        this.CUT = _ELEM_byTrnsfrm(this.STUB_Elem)
+    });
+    it(`should.be a Fn of arity:1 expecting an ELEM.`, () => {
+        _ELEM_byTrnsfrm.should.be.a('Function').and.is.length(1);
+    });
+    it(`should return a new Elem WHEN invoked w/ an Element`, function () {
+        this.CUT.should.be.an('Object')
+        // .and.have.property('style')
+            .and.deep.equal(
+            {style: {opacity: "0.5", backgroundColor: "blue"}}
+        )
+    });
+});
+describe(`{_ELEM_byTrnsfrm}::  (ELEM ) { _ELEM_byTrnsfrm( STUB_CSD) } -> ELEM 
+        `, function () {
 
-        this.STUB_CSD = {"opacity": "0.5", "bgColor": "green"};
-        this.STUB_TRNSFRMR = {
-            backgroundColor: R.always('yellow'),
-            opacity: R.always('0.5')
-        };
-        this.STUB_Elem = {style: {opacity: 1, bgColor: ''}};
-        // this.ELEM_byTrnsfrm = _ELEM_byTrnsfrm(this.STUB_TRNSFRMR)
+    //      (ELEM) {Fn:_ELEM(Fn:(Trnsfrm))} -> ELEM
+    let _ELEM_byTrnsfrm = require('../src/UPDATE_Elem')._byTrnsfrm;
+
+    beforeEach(function () {
+        // this.STUB_CSD = {"opacity": "0.5", "backgroundColor": "green"};
+        // this.STUB_TRNSFRMR = {
+        //     backgroundColor: R.always('yellow'),
+        //     opacity: R.always('0.5')
+        // };
+        this.STUB_Elem = {style: {opacity: 1, backgroundColor: ''}};
+        this.CUT = _ELEM_byTrnsfrm(this.STUB_Elem)
     });
-    it.only(`should.be a Fn of arity:1.`, () => {
+    it(`should.be a Fn of arity:1 expecting an ELEM.`, () => {
         _ELEM_byTrnsfrm.should.be.a('Function').and.is.length(1);
     });
-    it.skip(`should.be a Fn of arity:1 and Expect a CSD.`, () => {
-        _ELEM_byTrnsfrm.should.be.a('Function').and.is.length(1);
+    it(`should return a new Elem WHEN invoked w/ an Element`, function () {
+        this.CUT.should.be.an('Object')
+        // .and.have.property('style')
+            .and.deep.equal(
+            {style: {opacity: "0.5", backgroundColor: "blue"}}
+        )
     });
-    it.skip(`should produce a new Elem given a CSD`, function () {
-        this._ELEM_byTrnsfrm().should.isObject('Object')
-        // .and.have.property('opacity')
-        // .and.deep.equal({style: {opacity: "0.5", backgroundColor: "green"}
-    })
-})
-;
+});
