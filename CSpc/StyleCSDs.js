bdcf1b1ecccdb355ab4d6efd35342bed202d099f
clif edwards <clif.edwards.2@gmail.com>
2017-06-22 07:21:30 -0400

    [ StyleCSDs.js  ] ADDED

---
+++ CSpc/StyleCSDs.js
@@ -0,0 +1,7 @@
+/**
+ * StyleCSDs.js
+ */
+"use strict";
+
+module.exports.Dflt = {backgroundColor: '', opacity: '1.0', fontSize: '100%', color: ""};
+module.exports.Test = {color: "red", backgroundColor: 'blue', opacity: '.75', fontSize: '70%'};
