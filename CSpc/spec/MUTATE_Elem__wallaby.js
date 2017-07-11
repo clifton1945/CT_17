2a405ec1ebbc9aecf7b8c682b71fdbf9cf432269
clif edwards <clif.edwards.2@gmail.com>
2017-07-11 07:55:02 -0400

    [ MUTATE_Elem && __test.js ]  REFACT NAMING

--- CSpc/spec/MUTATE_Elem__wallaby.js
+++ CSpc/spec/MUTATE_Elem__wallaby.js
@@ -20,29 +20,36 @@ describe(`    MUTATE_:: mutates a span UPDATING its.style property.
     let STUB_ELEM = {style: {opacity: 1, color: ''}};
     // CODE UNDER TEST
     let MUTATE_ = require('../src/MUTATE_Elem').MUTATE_;
+    // a useful VerbForm of arity:1 is ...
     let MUTATE_frmCsdDCT = MUTATE_(STUB_CSD);
+    // as above but NounForm is ...
     let SERV_mutatedElem_frmCsdDCT = MUTATE_(STUB_CSD);
 
     beforeEach(function () {
         loadFixtures('index.html'); //REMEMBER this BREAKS a Mocha debug !!
     });
-
     describe(`{MUTATE_                      } Fn:(CsdDCT) ->Fn(ELEM -> ELEM)
         `, function () {
         it(`expects a Fn of arity:2; expecting a Span.`, () => {
             expect(MUTATE_).is.a('Function').and.is.length(2);
         });
     });
-    describe(`{MUTATE_frmCsdDCT = MUTATE_( CsdDCT )  } Fn(ELEM -> ELEM)
+    describe(`MUTATE_frmCsdDCT:arity:1 = MUTATE_( CsdDCT )    Fn(ELEM -> ELEM)
         `, function () {
         it(`expects a  Fn of arity:1; expecting a Span.`, () => {
             expect(MUTATE_frmCsdDCT).is.a('Function').and.is.length(1);
         });
-    });
-    describe(`{aka SERV_mutatedElem_frmCsdDCT === MUTATE_frmCsdDCT    } Fn(ELEM -> ELEM)
-        `, function () {
         it(`expects an Element Obj `, () => {
             expect(MUTATE_frmCsdDCT(STUB_ELEM)).is.a('Object')
+                .and.is.property('style')
+                .and.is.property("opacity", "0.5")
+            ;
+        });
+    });
+    describe(`SERV_mutatedElem_frmCsdDCT():arity:1 form is the same as MUTATE_frmCsdDCT() form of function.   } 
+        `, function () {
+        it(`expects an Element Obj `, () => {
+            expect(SERV_mutatedElem_frmCsdDCT(STUB_ELEM)).is.a('Object')
                 .and.is.property('style')
                 .and.is.property("opacity", "0.5")
             ;
