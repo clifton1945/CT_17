644a3c8c325ef70cedc31aa5cdb2837b00c32f65
clif edwards <clif.edwards.2@gmail.com>
2017-07-12 17:52:02 -0400

    [ MUTATE_Elem && __test.js ]   REFACT

--- CSpc/spec/MUTATE_Elem__wallaby.js
+++ CSpc/spec/MUTATE_Elem__wallaby.js
@@ -28,44 +28,45 @@ describe(`    MUTATE_:: mutates a span UPDATING its.style property.
     beforeEach(function () {
         loadFixtures('index.html'); //REMEMBER this BREAKS a Mocha debug !!
     });
-<<<<<<<
-    HEAD
-    describe(`{MUTATE_: arity:2                         } Fn:(CsdDCT) ->Fn(ELEM -> ELEM)
-=======
-    describe(`
-    {
-        MUTATE_
-    }
-    Fn:(CsdDCT) ->Fn(ELEM -> ELEM)
-        >>> >>> > origin / WIP
-            `, function () {
-        it(`expects a Fn of arity:2; expecting a Span.`, () => {
-            expect(MUTATE_).is.a('Function').and.is.length(2);
-        });
+});
+describe(`        MUTATE_ Fn:(CsdDCT) ->Fn(ELEM -> ELEM)
+    Fn:(CsdDCT) ->Fn(ELEM -> ELEM)            `, function () {
+    it(`expects a Fn of arity:2; expecting a Span.`, () => {
+        expect(MUTATE_).is.a('Function').and.is.length(2);
     });
-    describe(`MUTATE_frmCsdDCT:arity:1 = MUTATE_( CsdDCT )    Fn(ELEM -> ELEM)
+});
+describe(`MUTATE_frmCsdDCT:arity:1 = MUTATE_( CsdDCT )    Fn(ELEM -> ELEM)
         `, function () {
-        it(`expects a  Fn of arity:1; expecting a Span.`, () => {
-            expect(MUTATE_frmCsdDCT).is.a('Function').and.is.length(1);
-        });
-        it(`expects an Element Obj `, () => {
-            expect(MUTATE_frmCsdDCT(STUB_ELEM)).is.a('Object')
-                .and.is.property('style')
-                .and.is.property("opacity", "0.5")
-            ;
-        });
+    it(`expects a  Fn of arity:1; expecting a Span.`, () => {
+        expect(MUTATE_frmCsdDCT).is.a('Function').and.is.length(1);
     });
-<<<<<<< HEAD
-    describe(`SERV_mutatedElem_frmCsdDCT():arity:1      form is the same as MUTATE_frmCsdDCT() form of function.   }
-=== === =
-    describe(`SERV_mutatedElem_frmCsdDCT():arity:1 form is the same as MUTATE_frmCsdDCT() form of function.   } 
->>>>>>> origin/WIP
-        `, function () {
-        it(`expects an Element Obj `, () => {
-            expect(SERV_mutatedElem_frmCsdDCT(STUB_ELEM)).is.a('Object')
-                .and.is.property('style')
-                .and.is.property("opacity", "0.5")
-            ;
-        });
+    it(`expects an Element Obj `, () => {
+        expect(MUTATE_frmCsdDCT(STUB_ELEM)).is.a('Object')
+            .and.is.property('style')
+            .and.is.property("opacity", "0.5")
+        ;
+    });
+});
+describe(`SERV_mutatedElem_frmCsdDCT():arity:1      form is the same as MUTATE_frmCsdDCT() form of function.   }
+
+:
+arity:1
+form
+is
+the
+same as MUTATE_frmCsdDCT()
+form
+of
+
+function
+
+.
+}
+    `, function () {
+    it(` expects an Element Obj `, () => {
+        expect(SERV_mutatedElem_frmCsdDCT(STUB_ELEM)).is.a('Object')
+            .and.is.property('style')
+            .and.is.property("opacity", "0.5")
+        ;
     });
 });
