b221ad84d57d13187b7766c01ce5493c36473928
clif edwards <clif.edwards.2@gmail.com>
2017-07-27 07:25:59 -0400

    [ SRVa_RSpcCsd.js ]  STABLE after REFACT

--- RSpc/src/SRVa_WtFn.js
+++ RSpc/src/SRVa_WtFn.js
@@ -2,13 +2,24 @@
 
 let R = require('ramda')
     , curry = R.curry
-    // , prop = R.prop
-    // , indexOf = R.indexOf()
 ;
-let roundToTwoPlaces = require('../../h/roundToTwoPlaces');
+let roundTo_2 = require('../../h/roundToTwoPlaces').roundTo_2;
+let roundTo_3 = require('../../h/roundToTwoPlaces').roundTo_3;
+
 let weighterFn = curry( // note use ElemNdxLst -> elem, ndx, lst
-    (wtCnst, E, N, L) => roundToTwoPlaces(Math.pow(N / L.length, wtCnst))
+    /**
+     * Fn: weighterFn
+     * @param wtCnst B in x^^b
+     * @param E Element
+     * @param N Ndx
+     * @param L List
+     */
+    (wtCnst, E, N, L) => roundTo_2(
+        Math.pow(
+            (N / L.length), wtCnst)
+    )
 );
+
 let SRV_WtFn__GVNa_Cnst = curry( // actually this is SRV_WtFn__WTHa_Cnst__GVN_Ndx
     cnst => weighterFn(cnst)
 );
--- h/roundToTwoPlaces.js
+++ h/roundToTwoPlaces.js
@@ -6,7 +6,15 @@
 let R = require('ramda');
 let compose = R.compose;
 
-module.exports = compose(
+const roundTo_3 = compose(
+    R.divide(R.__, 1000), Math.round, R.multiply(1000)
+);// N -> N
+const roundTo_2 = compose(
     R.divide(R.__, 100), Math.round, R.multiply(100)
 );// N -> N
-// module.exports = roundToTwoPlaces;
+
+module.exports = compose(
+    R.divide(R.__, 100), Math.round, R.multiply(100)
+);// N -> N; // i.e. the default
+module.exports.roundTo_2 = roundTo_2;
+module.exports.roundTo_3 = roundTo_3;
