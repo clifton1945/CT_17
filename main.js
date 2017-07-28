28b433e8f57d8b72e63a84579c4a6b9090b38a60
clif edwards <clif.edwards.2@gmail.com>
2017-07-28 08:46:45 -0400

    [ main.js  ]  WIP  it is stable but just with Test STubs

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 /**
  *  170722 - concentrate of applying a NEW Weighted csd to a selected verse.
  *  Weighted being a Fn(elem, ndx, sibLst) of the element Index and its relative position in the parent.list of siblings.
- *  Here is my plan:
- *  Go down and
- *      (0) try R.mapObjectIndexed on the selected Item.
- *          alter the element.style.backgroundColor
- *      (1) get a sub list Chapter spans with index < 7.
- *          call them 'amList'
- *      (2)
- *      (3) apply a Fn:(elem, ndx, lst) to each elem in the amList.
+
+ *      apply a Fn:(elem, ndx, lst) to each elem in the amList.
  *          where Fn returns a shapeValue
  *              where W = n^^b
- *                   n = normalIndex: ndx/lst.length
+ *                   n = normalizedIndex: ndx/lst.length
  *                   b = some shape constant
  *                      such that W( b:0, n: 0->1 ) = 1. It is a constant:1
  *                      such that W( b:1, n: 0->1 ) = n. It is linear
@@ -31,13 +25,14 @@ let R = require('ramda')
 ;
 
 // ------- requires ------------
-let roundToTwoPlaces = require('./h/roundTo_');
-let SRVa_WtFn__GVNa_Cnst = require('./RSpc/src/SRVa_WtFn').SRV_WtFn__GVNa_Cnst;
-let EVOL_aCsd = require('./SSpc/src/EVOLVEa_Csd');
+// let roundToTwoPlaces = require('./h/roundTo_');
 let DfltCsd = require('./RSpc/Dflt_RSpcStyles').Dflt;
 let SRV_ChptVerses_Dflt = require('./CSpc/src/SRVa_ChptVerseList').SRV_ChptVerses_Dflt;
+let SRVa_WtFn__GVNa_Cnst = require('./RSpc/src/SRVa_WtFn').SRV_WtFn__GVNa_Cnst;
+
+let EVOL_aCsd = require('./SSpc/src/EVOLVEa_Csd');
 let SRVa_TRNFRM = require('./SSpc/src/SRVa_TRNFRM').by_always; // key -> valu -> Fn:TRNFRM
-let SRVa_Span = require('./CSpc/src/MUTATE_Elem').SRVa_Span__WTHa_Csd__GVNa_Span;
+// let SRVa_Span = require('./CSpc/src/MUTATE_Elem').SRVa_Span__WTHa_Csd__GVNa_Span;
 
 // -------- main starts here -------------
 let main;
@@ -89,19 +84,16 @@ main = function (item) {
 
 
     let modFn25 = SRVa_WtFn__GVNa_Cnst(0.25); // Fn( el, ndx, lst ) -> TrnsfrmObj
-    // FIX modFn25 IS NOT USED YET; using STUB_Trnsfrm below
-    let Fn = R.curry(
-        (el, ndx, lst) => {
-            (modFn25(R.__, ndx, lst), R.toString, SRVa_TRNFRM('opacity'))
-        }
-    ); // -> Fn:(-> n
+
+    // FIX modFn25 IS NOT USED YET; using STUB_Trnsfrm above
+    let X = modFn25({}, 0, [0, 1, 2, 3]);
 
 
 // Fn: SRV_mutatedElem  USING testCsd!!
     let SRV_mutatedElem = require('./CSpc/src/MUTATE_Elem').SRVa_(testCsd);
     //       csdDCT -> Fn(  eltDCT -> eltDCT )
 
-// CODE UNDER TEST
+// CODE UNDER TEST: STABLE
     let ret = SRV_mutatedElem(noonVerse);
     // NOW see the selected noonVerse info
     C_in_Both(`     The selected Verse is Index[${ n}]
