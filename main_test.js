4a82f2eb3b5d83e9c2dd0ec903ad437a6f30e12a
clif edwards <clif.edwards.2@gmail.com>
2017-08-11 11:37:30 -0400

    [ main_test.js  ] STABLE:begin w/ a simple test framework: node assert and mocha

--- main_test.js
+++ main_test.js
@@ -1,7 +1,7 @@
 /**
- * today: convert Fn: triage_aTrnfrmDct to evolving a style CSD: the true use of this function
+ * today: convert Fn: srva_Trnfrm_Dct to evolving a style CSD: the true use of this function
  *  OK @0815 (1) show and prove the returned DCT works with R.evolve
- *  (2) demonstrate that an Element.style.color can be evolved GVN focusIndex and elementIndex
+ *  OK @1008(2) demonstrate that an Element.style.color can be evolved GVN focusIndex and elementIndex
  */
 "use strict";
 
@@ -19,12 +19,15 @@ let R = require('ramda')
 const assert = require('assert');
 
 // CODE UNDER TEST
-let triage_aTrnfrmDct = R.curry(
+let srva_Trnfrm_Dct = R.curry(
     /**
+     * NOTE  JUST A STUB TODO exand with parameters
      * returns a Trnfrm_DCT an Elem
      * @param vtr:      the Index of the focus Elem
      * @param ndx:      the Index of a Elem
-     * @return dct:     a TrnfrmDCT
+     * @return dct:     a DCT:
+     *      key: a style Attribute
+     *      val: a Fn returning a style value
      */
     (vtr, ndx) => (ndx < vtr)
         ? {color: R.always('red')}
@@ -35,15 +38,22 @@ let triage_aTrnfrmDct = R.curry(
 
 describe(` an invoked triage_aTrnfrmDct(n1, n2) RETURNS DCT object of key:trnfrm transformations. `, function () {
     it('should return one of three transforms when INVOKED:   past, current, future', function () {
-        let cut = triage_aTrnfrmDct(5, 0).color;
-        let tst = R.pipe(R.split(' '), R.last)(cut());
-        assert.equal(tst, 'past', 'triage_aTrnfrmDct(5,0) should be already read');
+        let cut = R.curry(srva_Trnfrm_Dct(5));
+        let tst = cut(0).color();
+        assert.equal(
+            tst,
+            'red',
+            'srva_Trnfrm_Dct(5)(0) should be already read: red');
+        tst = cut(5).color();
+        assert.equal(tst, 'blue', 'srva_Trnfrm_Dct(5,5) should be currently reading');
+        tst = cut(6).color();
+        assert.equal(tst, 'green', 'srva_Trnfrm_Dct(5,60) should be will read');
     });
 });
 describe(`evolve(triage_aTrnfrmDct(n1, n2), someObject) RETURNS a new someObject. `, function () {
     it(`should return a modified Object `, function () {
         let cut = R.evolve(
-            triage_aTrnfrmDct(5, 0),
+            srva_Trnfrm_Dct(5, 0),
             {color: 'original'} // NOTE: this Attr will be changed!
         );
         let tst = ( typeof cut === 'object' );
@@ -51,8 +61,8 @@ describe(`evolve(triage_aTrnfrmDct(n1, n2), someObject) RETURNS a new someObject
     });
     it(`should return a modified Object.color != 'original `, function () {
         let cut = R.evolve(
-            triage_aTrnfrmDct(5, 0),
-            {color: 'original'}
+            srva_Trnfrm_Dct(5, 0),
+            {color: 'oyyyyriginal'}
         );
         let tst = cut.color;
         assert.notEqual(tst, 'original', `expect returned DCT != 'original'`);
