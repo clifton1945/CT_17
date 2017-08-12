0d24eec67dc4a02bc596b2e0a8e05010cc677ff1
clif edwards <clif.edwards.2@gmail.com>
2017-08-12 08:24:28 -0400

    [ aTest.js ] STABLE:  OK (4) iterate over some subset of verseSPANS to demonstrate srva_TrnfrmDCT    can alter a verseSpan styleCSD on the fly

--- aTest.js
+++ aTest.js
@@ -1,6 +1,6 @@
 /**
  * today: convert Fn: srva_Trnfrm_Dct to evolving a style CSD: the true use of this function
- *  (4) iterate over some subset of verseSPANS to demonstrate srva_TrnfrmDCT    can alter a verseSpan styleCSD on the fly.
+ *  OK (4) iterate over some subset of verseSPANS to demonstrate srva_TrnfrmDCT    can alter a verseSpan styleCSD on the fly.
  *  OK (1) show and prove the returned DCT works with R.evolve
  *  OK (2) demonstrate that an Element.style.color can be evolved GVN focusIndex and elementIndex
  *  OK (3) use Fn:srva_TrnfrmDCT_color(ndx, ndx) to prepare for iterating over all the chptDIV verseSPANS
@@ -32,22 +32,28 @@ let ChptDIV = document.querySelector('.chpt');
 ChptDIV.addEventListener("click", CLICK_VerseToRead, false);
 
 
-let main = function (vtr) { // vtr:VerseToRead
+let main = function (aVTR) { // aVTR:VerseToRead
 
 // ************** MAIN ********
     let TRK = "wbSample/main.js";
-    C_in_Console('  IN> ' + TRK);
+    C_in_Console('IN> ' + TRK);
 
-// use the vtr Span to derive some data constants
-    let VersesColl = vtr.parentElement.children;
-    let vtrNdx = R.indexOf(vtr, VersesColl);
-    C_in_Console(`  > VerseToRead.Index: ${vtrNdx}`);
+// use the aVTR Span to derive some data constants
+    let versesCOLL = aVTR.parentElement.children;
+    let vtrNdx = R.indexOf(aVTR, versesCOLL);
 
-    let aCsd = R.evolve(srva_TrnfrmDCT_color(vtrNdx, vtrNdx), {color: ''});
+// mutate each verseELT by evolving a CSD
+    R.addIndex(R.map)(
+        (e, n, a) => {
+            // evolve a CSD
+            let aCSD = R.evolve(srva_TrnfrmDCT_color(vtrNdx, n), {color: ''});
+            // C_in_Console(`  > VerseToRead.Index: ${vtrNdx}`);
 
-// now with a style.Csd, mutate the vtr Element
-    let ret = mutate_anElem(aCsd, vtr);
-    C_in_Console(`  > style.color: ${ret.style.color}`);
+            // now with a style.Csd, mutate the aVTR Element
+            let ret = mutate_anElem(aCSD, e);
+            // C_in_Console(`  > element.style.color: ${ret.style.color}`);
+        }
+    )(versesCOLL);
 
-    C_in_Console(' OUT> ' + TRK);
+    C_in_Console('OUT> ' + TRK);
 };
