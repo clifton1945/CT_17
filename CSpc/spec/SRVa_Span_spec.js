f1643ba290dce4646e17d11f436b355539a366e6
clif edwards <clif.edwards.2@gmail.com>
2017-08-21 18:32:06 -0400

    [ SRVA_Span.js ] STABLE:  with a few beginning R.pipes

--- CSpc/spec/SRVa_Span_spec.js
+++ CSpc/spec/SRVa_Span_spec.js
@@ -11,22 +11,21 @@ let chai = require('chai')
 // NEW CODE UNDER TEST -
 let srva_ChptDIV = R.pipe(R.invoker(1, 'querySelector')
     ('.chpt')
-);// (document)Fn->(div)
+);// (Doc)Fn->(div)
 let srva_VerseColl = R.pipe(
     R.invoker(1, 'querySelector')('.chpt'),
     R.prop('children')
-);// (document)Fn->(Coll)
+);// (Doc)Fn->(Coll)
 let srva_ChptSpan = R.pipe(
     R.invoker(1, 'querySelector')
     ('.chpt'),
     R.prop('firstElementChild')
-);// (document)Fn->(Span)
-let srva_SpanNdx = R.pipe(
-    R.always(7)
-); // (span)Fn->(ndx)
+);// (Doc)Fn->(Span)
+let srva_SpanNdx = span => R.indexOf(span, span.parentElement.children)
+; // (Span)Fn->(Ndx)
 
 
-describe(` Fn: SRVa_Span from a document 
+describe(` Fn: SRVa_Span from a Doc: (Doc)Fn->(Span) 
         `, function () {
     beforeEach(function () {
         loadFixtures('index.html');
@@ -35,11 +34,24 @@ describe(` Fn: SRVa_Span from a document
         this.coll = srva_VerseColl(document);
 
     });
-    it(`Fn: SRVa_Span should have a default SPAN: 
-            typically the first div.childElement`
-        , function () {
+    it(`Fn: SRVa_Span from a Doc: 
+            typically the first div.childElement
+            `, function () {
             expect(this.span.tagName).is.equal('SPAN');
             expect(R.indexOf(this.span, this.coll)).is.equal(0)
+        }
+    );
+});
+describe(` Fn: SRVa_SpanNdx from a Span: (Span)Fn->(Ndx) 
+        `, function () {
+    beforeEach(function () {
+        loadFixtures('index.html');
+        this.span = srva_ChptSpan(document);
+    });
+    it(`Fn:(Span)Fn->(Ndx) SRVa_SpanNdx returns a index NUM of its peer position. 
+     `, function () {
+            expect(this.span.tagName).is.equal('SPAN');
+        expect(R.indexOf(this.span, this.span.parentElement.children)).is.equal(0)
         }
     );
 });
