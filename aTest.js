01d8d6fa0665334814884dc35f236a5bd8fb1ce7
clif edwards <clif.edwards.2@gmail.com>
2017-08-22 13:39:20 -0400

    [ aTest.js ]  STABLE  before WIP

--- aTest.js
+++ aTest.js
@@ -1,33 +1,27 @@
 /**
- * 2017/08/22 @
- *  confirm
  */
 "use strict";
 // ------- requires ------------
 let R = require('ramda');
-// , curry = R.curry
-// , concat = R.concat
-// , pipe = R.pipe
-// , evolve = R.evolve
 let C_in = require('./h/C_in_')
     , C_in_Console = C_in.Console
-    , C_in_Both = C_in.Both
-;
-let myTap = require('./h/myTap')
-;
-let assert = require('assert')
-;
+    , C_in_Both = C_in.Both;
 
-let srva_TrnfrmDCT_color;// Num -> Num -> {k:FN}
-srva_TrnfrmDCT_color = require('./SSpc/src/SRVa_TrnfrmDCT').colorStyleTrnfrmDCT;
+// ------- CodeUnderTest requires
+let srva_TrnfrmDCT_color =
+    require('./SSpc/src/SRVa_TrnfrmDCT').colorStyleTrnfrmDCT;
+let mutate_anElem =
+    require('./CSpc/src/MUTATE_Elem').MUTATE_;
 
-let mutate_anElem;// CSD -> ( ELEM -> ELEM )
-mutate_anElem = require('./CSpc/src/MUTATE_Elem').MUTATE_;
+// -------- main starts here -------------
 
-// -------- main FUNCTIONS -------------
+// select a DIV in the DOM as theLight
+let ChptDIV;
+let selectFrom = R.invoker(1, 'querySelector');
+ChptDIV = selectFrom('.chpt')(document);//Number → String → (a → b → … → n → Object → *)
 
 
-// This is the MouseEvent handler to select a readFocus span a.k.a theLight
+// This is the MouseEvent handler to select a readFocus span.
 function CLICK_VerseToRead(e) {
     if (e.target !== e.currentTarget) {
         e.stopPropagation();
@@ -35,51 +29,30 @@ function CLICK_VerseToRead(e) {
     }
     e.stopPropagation();
 }
-// SET the EVENT LISTENER in the ChprDIV:: the Light
-srva_ChptDIV(document).addEventListener("click", CLICK_VerseToRead, false);
 
+ChptDIV.addEventListener("click", CLICK_VerseToRead, false);
+
+// ************** MAIN ********
+let TRK = "wbSample/main.js";
+C_in_Console('IN> ' + TRK);
 
 let main = function (aVTR) { // aVTR:VerseToRead
 
-// ************** MAIN ********
-    let TRK = "aTest.js";
-    C_in_Console('IN> ' + TRK);
-
 // use the aVTR Span to derive some data constants
-    let versesCOLL;
-    versesCOLL = srva_VerseColl(aVTR);
-    // versesCOLL = aVTR.parentElement.children;
-    let vtrNdx = srva_SpanNdx(aVTR);
-    C_in_Both(`  > VerseToRead.Ndx: ${vtrNdx}`);
+    let versesCOLL = aVTR.parentElement.children;
+    let vtrNdx = R.indexOf(aVTR, versesCOLL);
 
-
-// use the default style DCT with on arbitrarily stubbed
-    let dfltCsds = require('./SSpc/StyleCSDS');
-
-// need a srva_CSD(vtrNdx) for each Verse now that a VerseToRead::vtrNdx is available.
-    let srva_CSD = R.curry(
-        /**
-         * FN: srva_CSD EVOLVES & SERVES a styleCsd for this Element
-         * @param vtr_ndx
-         * @param e_ndx
-         */
-        (vtr_ndx, e_ndx) => R.evolve(// {k: (v → v)} → {k: v} → {k: v}
-            srva_TrnfrmDCT_color(vtr_ndx, e_ndx),
-            dfltCsds.noon
-        ))(vtrNdx); // NOTE vtrNdx is invoked here.
-
-    /*
-     * mutate each verseElem using an evolved CSD:
-     * NOTE: the evolved CSD is using a STUB:  dfltCsds.noon
-    */
+// mutate each verseELT by evolving a CSD
     R.addIndex(R.map)(
         (e, n, a) => {
             // evolve a CSD
             let aCSD = R.evolve(srva_TrnfrmDCT_color(vtrNdx, n), {color: ''});
             C_in_Both(`  > VerseToRead.Index: ${vtrNdx}`);
 
-            // now with a style.Csd, mutate the aVTR Element
-            let ret = mutate_anElem(aCSD, e);
+            // now with an evolved style.Csd, mutate the aVTR Element
+            mutate_anElem(aCSD, e);
+            // let ret = mutate_anElem(aCSD, e);
+            // C_in_Console(`  > element.style.color: ${ret.style.color}`);
         }
     )(versesCOLL);
 
