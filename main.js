7d0e5b15f8013fb16be524741a1bfa5351ab0273
clif edwards <clif.edwards.2@gmail.com>
2017-08-22 13:36:33 -0400

    [ main.js ] STABLE:  REFACT

--- main.js
+++ main.js
@@ -1,26 +1,17 @@
 /**
- * today: use Fn: srva_Trnfrm_Dct to evolving a style CSD for each verse.
- * () maybe practice some compose/pipe with existing
- * () start piping in more TrnfrmDCTs before the final R.al
- * () add more Attributes to the final CSD
- *  OK (4) iterate over some subset of verseSPANS to demonstrate srva_TrnfrmDCT    can alter a verseSpan styleCSD on the fly.
- *  OK (3) use Fn:srva_TrnfrmDCT_color(ndx, ndx) to prepare for iterating over all the chptDIV verseSPANS
- *  OK (2) demonstrate that an Element.style.color can be evolved GVN focusIndex and elementIndex
- *  OK (1) show and prove the returned DCT works with R.evolve
  */
 "use strict";
 // ------- requires ------------
 let R = require('ramda');
-// , curry = R.curry
-// , concat = R.concat
-// , pipe = R.pipe
-// , evolve = R.evolve
 let C_in = require('./h/C_in_')
     , C_in_Console = C_in.Console
     , C_in_Both = C_in.Both;
+
+// ------- CodeUnderTest requires
 let srva_TrnfrmDCT_color =
     require('./SSpc/src/SRVa_TrnfrmDCT').colorStyleTrnfrmDCT;
-let mutate_anElem = require('./CSpc/src/MUTATE_Elem').MUTATE_;//Fn
+let mutate_anElem =
+    require('./CSpc/src/MUTATE_Elem').MUTATE_;
 
 // -------- main starts here -------------
 
@@ -55,10 +46,11 @@ let main = function (aVTR) { // aVTR:VerseToRead
         (e, n, a) => {
             // evolve a CSD
             let aCSD = R.evolve(srva_TrnfrmDCT_color(vtrNdx, n), {color: ''});
-            // C_in_Console(`  > VerseToRead.Index: ${vtrNdx}`);
+            C_in_Both(`  > VerseToRead.Index: ${vtrNdx}`);
 
-            // now with a style.Csd, mutate the aVTR Element
-            let ret = mutate_anElem(aCSD, e);
+            // now with an evolved style.Csd, mutate the aVTR Element
+            mutate_anElem(aCSD, e);
+            // let ret = mutate_anElem(aCSD, e);
             // C_in_Console(`  > element.style.color: ${ret.style.color}`);
         }
     )(versesCOLL);
