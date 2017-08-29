ea3aba0b3277b29d4a3acf9784b75fb632494562
clif edwards <clif.edwards.2@gmail.com>
2017-08-29 11:17:42 -0400

    [ aTest.js ] STABLE -- now using document.addEventListener...

--- aTest.js
+++ aTest.js
@@ -9,30 +9,21 @@ let C_in = require('./h/C_in_')
 ;
 // ************** MAIN ********
 let TRK = "wbSample/aTest.js";
-console.log('>  IN ' + TRK);
-// C_in_Console(' IN> ' + TRK);
+C_in_Console('> IN  ' + TRK);
 
 // ------- CodeUnderTest requires
-// let update_ChptSpans = require('./CSpc/src/UPDATE_CSpc').update_;
-
-// first init the index.html
-// let ChptDIV = document.getElementById('.chpt');
-
-// update_ChptSpans(ChptDIV.firstElementChild);
-// also set listener
-// USE this MouseEvent handler to select a readFocus span.
-// function CLICK_VerseToRead(e) {
-//     if (e.target !== e.currentTarget) {
-//         e.stopPropagation();
-//         update_ChptSpans(e.target);
-//     }
-//     e.stopPropagation();
-// }
-// ChptDIV.addEventListener('click', CLICK_VerseToRead, false);
+let update_ChptSpans = require('./CSpc/src/UPDATE_CSpc').update_;
+let CLICK_VerseToRead = (e) => {
+    if (e.target !== e.currentTarget) {
+        e.stopPropagation();
+        update_ChptSpans(e.target);
+    }
+    e.stopPropagation();
+};  // USE this MouseEvent handler to select a readFocus span.
 
 document.addEventListener("DOMContentLoaded", function (event) {
-    let ChptDIV = document.getElementById('.chpt', false);
-    console.log('this is the ChptDiv:' + ChptDIV);
+    let ChptDIV = document.querySelector('.chpt');
+    update_ChptSpans(ChptDIV.firstElementChild);
+    ChptDIV.addEventListener('click', CLICK_VerseToRead, false);
 });
-console.log('> OUT ' + TRK);
-// C_in_Console('OUT> ' + TRK);
+C_in_Console('> OUT> ' + TRK);
